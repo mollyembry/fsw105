9c30a98c16c1732fc2fba68ba0353333cf28694d
mollyembry <molly.embry@bryanuniversity.edu>
2020-12-07 21:48:02 -0500

    es6

--- week5/es6/index.js
+++ week5/es6/index.js
@@ -1,7 +1,8 @@
+// Number 1
 const person = "John";
 const age = 101;
 function runForLoop(pets) {
-    var petObjects = [];
+    const petObjects = [];
     for ( let i = 0; i < pets.length; i++) {
         let pet = { type: pets[i] };
         if (pets[i] === "cat") {
@@ -17,3 +18,83 @@ function runForLoop(pets) {
     return petObjects
 }
 runForLoop(["cat", "dog"]);
+//Number 2
+const carrots =["bright orange", "ripe", "rotten"];
+
+mapVegetables = arr => {
+    return arr.map(function(carrot) {
+        return { type: "carrot", name: carrot};
+    });
+};
+console.log(carrots);
+//Number 3
+const people = [{
+    name: "Princess Peach",
+    friendly: false
+},
+{
+    name: "Luigi",
+    friendly: true
+},
+{   name: "Mario",
+    friendly: true
+
+},
+{
+    name: "Bowser",
+    friendly: false
+}
+];
+filterForFriendly = arr => {
+    return arr.filter(function(person) {
+        return person.friendly
+    });
+};
+console.log(filterForFriendly(people));
+//Number 4
+doMathSum = (a, b) => a + b;
+console.log(doMathSum(5, 2));
+ 
+produceProduct = (a,b) => a* b;
+console.log(produceProduct(5, 2));
+//Number5
+function printString(firstName, lastName, age) {
+    firstName = "Jane";
+    lastName = "Doe";
+    age = 100;
+    return("Hi " + firstName + " " + lastName + " , how does it feel to be " + age + "?");
+}
+    console.log(printString());
+//Number 6
+function printString(firstName, lastName, age) {
+    firstName = "Jane";
+    lastName = "Doe";
+    age = 100;
+    return(` Hi ${firstName} ${lastName}, how does it feel to be ${age}?`);
+}
+console.log(printString());
+//Number 7
+const animals = [
+    {type: "dog", name: "theodore" },
+    {type: "cat", name: "whiskers"},
+    { type: "pig", name: "piglette" },
+    { type: "dog", name: "sparky"}
+];
+filterForDogs = arr => arr.filter ( animal => (animal.type === "dog") ? true :false);
+  console.log(filterForDogs([
+    {type: "dog", name: "theodore" },
+    {type: "cat", name: "whiskers"},
+    { type: "pig", name: "piglette" },
+    { type: "dog", name: "sparky"}
+
+]));
+//Number 8
+function whereWhoString(location, name) {
+    return (`Hello ${name}! \n Welcome to ${location}. \n I hope you enjoy your stay, please ask the President of ${location} if you need anything.`);
+}
+console.log(whereWhoString("Hawaii", "Janice"));
+
+
+
+
+    
